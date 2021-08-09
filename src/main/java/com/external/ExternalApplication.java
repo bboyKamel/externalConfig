32284ce6b8ec272ce0d18c34fa113c5511c771dc
Przemek Przybylski <przemyslaw.a.przybylski@capgemini.com>
2021-08-09 07:58:01 +0200

    redudndant cde rollback

--- src/main/java/com/external/ExternalApplication.java
+++ src/main/java/com/external/ExternalApplication.java
@@ -1,23 +1,10 @@
 package com.external;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-@Configuration
-public class ExternalApplication implements WebApplicationInitializer {
-
-        @Override
-        public void onStartup(ServletContext servletContext) throws ServletException {
-            System.out.println("Ustawiam Profil");
-            servletContext.setInitParameter("spring.profiles.active", "DEV");
-        }
+public class ExternalApplication {
         
 	public static void main(String[] args) { 
 		SpringApplication.run(ExternalApplication.class, args);     
--- src/main/java/com/external/config/ApplicationConfig.java
+++ src/main/java/com/external/config/ApplicationConfig.java
@@ -3,7 +3,6 @@ package com.external.config;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.WebApplicationInitializer;
 
@@ -12,7 +11,6 @@ public class ApplicationConfig implements WebApplicationInitializer {
     
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        System.out.println("Ustawiam Profil");
         servletContext.setInitParameter("spring.profiles.active", "DEV");
     }
 }
