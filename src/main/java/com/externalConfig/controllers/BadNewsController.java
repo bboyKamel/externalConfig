2a21c4fb035c359102a0a0682b2faf8347857038
Przemek Przybylski <prprzyby@LCE45631.corp.capgemini.com>
2021-08-18 07:58:37 +0200

    refactor: remove environment

--- src/main/java/com/externalConfig/controllers/BadNewsController.java
+++ src/main/java/com/externalConfig/controllers/BadNewsController.java
@@ -2,9 +2,7 @@
 package com.externalConfig.controllers;
 
 import com.externalConfig.model.AnswerDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,15 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BadNewsController {
     
-    @Autowired
-    private Environment env;
+    @Value("${app.news.bad.message}")
+    private String badMessage;
     
     @Value("${app.news.bad.path}")
     private String badNewsPath;
     
     @GetMapping(path = "/bad-news", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity badNews() throws InterruptedException {
-        AnswerDTO answer = new AnswerDTO(badNewsPath, env.getProperty("app.news.bad.message"));
+        AnswerDTO answer = new AnswerDTO(badNewsPath, badMessage);
         Thread.sleep(5000);
     return new ResponseEntity(answer, HttpStatus.OK);
 
--- src/main/java/com/externalConfig/controllers/GoodNewsController.java
+++ src/main/java/com/externalConfig/controllers/GoodNewsController.java
@@ -2,9 +2,7 @@
 package com.externalConfig.controllers;
 
 import com.externalConfig.model.AnswerDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,15 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GoodNewsController {
     
-    @Autowired
-    private Environment env;
+    @Value("${app.news.good.message}")
+    private String goodMessage;
     
     @Value("${app.news.good.path}")
     private String goodNewsPath;
     
     @GetMapping(path = "/good-news", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity goodNews() {   
-        AnswerDTO answer = new AnswerDTO(goodNewsPath, env.getProperty("app.news.good.message"));
+        AnswerDTO answer = new AnswerDTO(goodNewsPath, goodMessage);
     
     return new ResponseEntity(answer, HttpStatus.OK);
     }
